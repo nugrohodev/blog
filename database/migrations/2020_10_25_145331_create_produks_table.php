c604e2726fc2a3401be2398785f2e790f08354cc
nugrohodev <you@example.com>
2020-10-26 11:03:14 +0700

    pertama cuy

---
+++ database/migrations/2020_10_25_145331_create_produks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->integer('harga')->default(150000);
+            $table->integer('harga_nameset')->default(50000);;
+            $table->boolean('is_ready')->default(true);
+            $table->string('jenis');
+            $table->float('berat')->default(0.5);;
+            $table->string('gambar');
+            $table->integer('liga_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produks');
+    }
+}
